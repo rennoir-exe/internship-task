6bd1abad14a46536b9b5e51fd9ca02e84caa1381
Renata Arun Belli <5024108@it.fcrit.ac.in>
2026-06-23 22:04:40 +0530

    Completed Laravel internship task

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Internship Task</title>
+
+    <style>
+        body{
+            margin:0;
+            height:100vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            background:linear-gradient(135deg,#4f46e5,#9333ea);
+            font-family:Arial,sans-serif;
+        }
+
+        .card{
+            background:white;
+            padding:40px;
+            border-radius:20px;
+            box-shadow:0 10px 30px rgba(0,0,0,0.2);
+            text-align:center;
+            width:500px;
+        }
+
+        h1{
+            color:#4f46e5;
+            margin-bottom:20px;
+        }
+
+        h2{
+            color:#333;
+        }
+
+        .success{
+            color:green;
+            font-weight:bold;
+            margin-top:20px;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="card">
+    <h1>🚀 Laravel Internship Task</h1>
+
+    <h2>Name: Renata Belli</h2>
+
+    <h2>Roll Number: 5024108</h2>
+
+    <p class="success">
+        ✅ Successfully Modified Laravel Homepage
+    </p>
+
+    <hr>
+
+    <p>
+        Built using Laravel, PHP and GitHub
+    </p>
+</div>
+
+</body>
+</html>
